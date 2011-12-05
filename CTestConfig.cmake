cbf2d2752e13b7df12a150b9d6a0b02667229fa6
Adrien Oliva (Admin) <adrien.oliva@gmail.com>
2011-12-05 15:45:52 +0100

    RMID #16: Add CDash Support
    Configure CTest with the correct variable.

---
+++ CTestConfig.cmake
@@ -0,0 +1,9 @@
+set (CTEST_NIGHTLY_START_TIME "20:30:00 GMT+1")
+
+set (MODEL "Weekly")
+set (CTEST_PROJECT_NAME "libavl")
+set (CTEST_DROP_SITE "sd-33166.dedibox.fr")
+set (CTEST_DROP_LOCATION "/CDash/submit.php?project=libavl")
+set (CTEST_DROP_SITE_CDASH TRUE)
+
+
