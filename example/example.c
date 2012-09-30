9b29b7f88155c333a555c552ad64a221802afdc8
Adrien Oliva <adrien.oliva@gmail.com>
2012-09-30 15:04:41 +0200

    Correct warning at compile time for example

--- example/example.c
+++ example/example.c
@@ -44,11 +44,14 @@ struct data {
      }
  }
 
- int main(int argc, char *argv)
+ int main(int argc, char **argv)
  {
      tree *avl_tree = NULL;
      struct data tmp;
-     int result;
+     unsigned result;
+
+     (void) argc;
+     (void) argv;
 
      // Initialize a new tree with our three previously defined
      // functions to store data structure.
@@ -60,6 +63,7 @@ struct data {
      // Add element {42, 4242} in our tree.
      result = insert_elmt(avl_tree, &tmp, sizeof(struct data));
      // Here result is equal to 1 since there is only 1 element in tree.
+     printf("Result after first insert: %d\n", result);
 
      // Dump tree to stdout with data_print function
      print_tree(avl_tree);
